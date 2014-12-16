a27459b4a125123c2d41c30149f9682d82157a23
fefa4ka <fefa4ka@gmail.com>
2014-12-17 01:59:40 +0300

    remove static folders

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -116,10 +116,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "../media"),
-    os.path.join(BASE_DIR, "../frontend/static"),
-)
+
 
 
 # Rest Framework
