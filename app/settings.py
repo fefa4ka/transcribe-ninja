6470d9103b9277e5bfcdefe52a59a2cb4065c168
fefa4ka <fefa4ka@fefa4ka-osx.local>
2014-12-23 23:16:13 +0300

    fix

--- app/settings.py
+++ app/settings.py
@@ -170,13 +170,12 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "frontend/bin/"),
-)
+
 if DEBUG:
     FRONTEND_ROOT = os.path.join(BASE_DIR, "frontend/build/")
     STATICFILES_DIRS = (
         FRONTEND_ROOT,
+        os.path.join(BASE_DIR, "frontend/bin/"),
     )
 
 STATIC_URL = '/static/'
