9a9ec2a3e13ed90e770d4a0a88fa1272101f28ab
fefa4ka <fefa4ka@fefa4ka-osx.local>
2015-06-03 08:25:53 +0300

    Main page initi

---
+++ frontend/src/app/main/main.js
@@ -0,0 +1,30 @@
+angular.module( 'transcribe-ninja.main', [
+  'transcribe-ninja.order',
+  'ui.router'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'main-page', {
+    url: '/',
+    views: {
+      "main": {
+        controller: 'RecordListCtrl',
+        templateUrl: 'main/main.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Transcribe.ninja – стенографирование разговоров' }
+  });
+})
+
+.controller( 'MainCtrl', function MainCtrl( $scope, $translate, $modal, $log, api ) {
+  $translate.use("ru");
+
+  $scope.upload = function() {
+    console.log('upload');
+  };
+
+
+
+})
+
+;
