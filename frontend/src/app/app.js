cf84d48d32cde42e730d5a38cf343a17efabb71d
fefa4ka <fefa4ka@fefa4ka-osx.local>
2015-06-03 08:28:13 +0300

    fix

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -24,7 +24,7 @@ angular.module( 'transcribe-ninja', [
 })
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/work' );
+  $urlRouterProvider.otherwise( '/' );
 
 })
 
