8758b6d8e82538a7175cb3b17ce0e3fe7a99b1b0
fefa4ka <fefa4ka@fefa4ka-osx.local>
2015-06-03 09:01:51 +0300

    logo as directive

--- frontend/src/app/main/main.js
+++ frontend/src/app/main/main.js
@@ -15,9 +15,46 @@ angular.module( 'transcribe-ninja.main', [
     data:{ pageTitle: 'Transcribe.ninja – стенографирование разговоров' }
   });
 })
-
-.controller( 'MainCtrl', function MainCtrl( $scope, $translate, $modal, $log, api ) {
+.directive('mainLogo', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'main/main.logo.tpl.html',
+    link: function (scope, element, attrs) {
+      console.log('make logo');
+    }
+  };
+})
+.controller( 'MainCtrl', function MainCtrl( $scope, $interval, $translate, $modal, $log, api ) {
   $translate.use("ru");
+
+  $scope.slogan_wave = {
+    amplitude: 10,
+    position: 30,
+    direction: 1
+  }
+          $sloganFirstPath = $("#sloganFirstPath"),
+          $sloganSecondPath = $("#sloganSecondPath");
+
+          
+  };
+
+  
+      wave = $interval(function(){
+    if(index > max) {
+            direction = -1;
+        }
+        if(index < min) {
+            direction = 1;
+        } 
+        
+        index = index + 2 * direction;
+        var index_2 = (index - 30),
+            index_4 = 220 + index,
+            index_5 = index;
+        
+        path.attr("d", "M0,44 C17,"+index+" 90,31 189,31 C189,30 200,0 200,0");
+        pathFirst.attr("d", "M0,44 C14,"+(index_2*-1)+" 69,19 101,27 C132,34 197,58 "+index_4+","+index_5);
+  });
 
   $scope.upload = function() {
     console.log('upload');
