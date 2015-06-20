5e7507b8a36d797beb8c3fe1af6abeed597274a7
fefa4ka <fefa4ka@fefa4ka-osx.local>
2015-06-20 18:19:16 +0300

    history fix

--- api/views.py
+++ api/views.py
@@ -330,14 +330,17 @@ class HistoryViewSet(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticated,
                           IsOwner,)
     pagination_class = StandartResultSetPagination
-    filter_backends = (filters.DjangoFilterBackend,)
-    filter_class = HistoryFilter
+    # filter_backends = (filters.DjangoFilterBackend,)
+    # filter_class = HistoryFilter
 
     def get_queryset(self):
         user = self.request.user
+        print self.request.QUERY_PARAMS
         if 'checked' in self.request.QUERY_PARAMS:
+            print "CHECKED"
             return user.account.queues(unchecked=False).order_by('-checked')
-        elif 'unchecked' in self.request.QUERY_PARAMS:
+
+        if 'unchecked' in self.request.QUERY_PARAMS:
             return user.account.queues(unchecked=True).order_by('-completed')
 
         return user.queue.filter(completed__isnull=False)
@@ -358,37 +361,39 @@ class StatisticsView(APIView):
         }
         queues = None
 
-    # try:
+        try:
 
-        if 'unchecked' in self.request.QUERY_PARAMS:
-            queues = account.queues(unchecked=True, after_date=after_date).order_by('-completed')
+            if 'unchecked' in self.request.QUERY_PARAMS:
+                queues = account.queues(unchecked=True, after_date=after_date).order_by('-completed')
 
-        if 'checked' in self.request.QUERY_PARAMS:
-            queues = account.queues(unchecked=False, after_date=after_date).order_by('-checked')
+            if 'checked' in self.request.QUERY_PARAMS:
+                queues = account.queues(unchecked=False, after_date=after_date).order_by('-checked')
 
-        if not queues:
-            if after_date:
-                queues = request.user.queue.filter(completed__gt=after_date)
-            else:
-                queues = request.user.queue.filter(completed__isnull=False)
+            if not queues:
+                if after_date:
+                    queues = request.user.queue.filter(completed__gt=after_date)
+                else:
+                    queues = request.user.queue.filter(completed__isnull=False)
 
-            checked_queues = account.queues(unchecked=False, after_date=after_date).order_by('-checked')
-            for queue in checked_queues:
-                statistics["checked_length"] += queue.work_length - queue.mistakes_length
+                checked_queues = account.queues(unchecked=False, after_date=after_date).order_by('-checked')
+                for queue in checked_queues:
+                    statistics["checked_length"] += queue.work_length - queue.mistakes_length
 
-        for queue in queues:
-            statistics["work_length"] += queue.work_length
-            statistics["mistakes_length"] += queue.mistakes_length
+            for queue in queues:
+                statistics["work_length"] += queue.work_length
+                statistics["mistakes_length"] += queue.mistakes_length
 
+                if 'checked' in self.request.QUERY_PARAMS:
+                    statistics["checked_length"] += queue.work_length - queue.mistakes_length
 
-            if queue.work_type == Queue.TRANSCRIBE:
-                statistics["duration_transcribe"] += queue.duration
-            elif queue.work_type == Queue.EDIT:
-                statistics["duration_check"] += queue.duration
+                if queue.work_type == Queue.TRANSCRIBE:
+                    statistics["duration_transcribe"] += queue.duration
+                elif queue.work_type == Queue.EDIT:
+                    statistics["duration_check"] += queue.duration
 
-        return Response(statistics)
-        # except:
-        #     return Response({"success": False})
+            return Response(statistics)
+        except:
+            return Response({"success": False})
 
 
 class TranscriptionViewSet(viewsets.ModelViewSet):
