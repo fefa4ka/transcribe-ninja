c9208d803474952d85f15d2c8f9c2627ffed5401
fefa4ka <fefa4ka@fefa4ka-osx.local>
2015-07-01 11:24:01 +0300

    basic auth fix

--- api/account/authentication.py
+++ api/account/authentication.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from rest_framework.authentication import BasicAuthentication
 
+
 class QuietBasicAuthentication(BasicAuthentication):
     # disclaimer: once the user is logged in, this should NOT be used as a
     # substitute for SessionAuthentication, which uses the django session cookie,
