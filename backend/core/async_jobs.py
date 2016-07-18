e7d995e2464ea99bc42e3bc3752c31d3c71ea98e
fefa4ka <fefa4ka@gmail.com>
2016-07-19 01:54:25 +0300

    fix mistake

--- backend/core/async_jobs.py
+++ backend/core/async_jobs.py
@@ -90,7 +90,7 @@ def make_queue(order):
         order - экземпляр класса заказа Order
     """
     # Пока не диаризируется, не создавать очередь
-    order = order.objects.get(id=record.id)
+    order = order.objects.get(id=order.id)
     if order.record.progress >= Record.PROGRESS_DIARIZED:
         order.record.progress = Record.PROGRESS_INWORK
         order.record.save()
